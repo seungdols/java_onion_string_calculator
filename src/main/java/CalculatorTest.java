23eb6f06dcf000f3990757099c2fa105ee5ba5d6
seungdols <seungdols0822@gmail.com>
2017-07-30 13:52:54 +0900

    add: Test 코드 추가

---
+++ src/main/java/CalculatorTest.java
@@ -0,0 +1,16 @@
+/**
+ * Created by seungdols on 2017. 7. 30..
+ */
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+
+		Calculator calculator = new Calculator();
+		System.out.println(calculator.add(3, 4));
+		System.out.println(calculator.subtract(3, 4));
+		System.out.println(calculator.multiply(3, 4));
+		System.out.println(calculator.devide(9, 3));
+
+	}
+
+}
