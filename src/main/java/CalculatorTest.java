caa77a8f434e713187c9b85be7fedf0a6906e102
seungdols <seungdols0822@gmail.com>
2017-07-30 13:58:11 +0900

    update: 테스트 코드의 메소드 분리

--- src/main/java/CalculatorTest.java
+++ src/main/java/CalculatorTest.java
@@ -6,9 +6,28 @@ public class CalculatorTest {
 	public static void main(String[] args) {
 
 		Calculator calculator = new Calculator();
+		add(calculator);
+		subtract(calculator);
+		multiply(calculator);
+		divide(calculator);
+
+	}
+
+	private static void add(Calculator calculator) {
 		System.out.println(calculator.add(3, 4));
+
+	}
+
+	private static void subtract(Calculator calculator) {
 		System.out.println(calculator.subtract(3, 4));
+
+	}
+
+	private static void multiply(Calculator calculator) {
 		System.out.println(calculator.multiply(3, 4));
+	}
+
+	private static void divide(Calculator calculator) {
 		System.out.println(calculator.devide(9, 3));
 
 	}
